9dd4b56c6606350bb7924b38723ea295b7210d61
BacteriaQH <hoangphuc2k1vp@gmail.com>
2022-10-18 22:41:10 +0700

    init app react-native

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 function App() {
-    return <div className="App">TeZa</div>;
+    const [data, setData] = useState([]);
+    const handleData = () => {
+        setData(['Banana', 'Orange', 'Apple', 'Mango']);
+        console.log(data);
+    };
+    const deleteData = (i) => {
+        data.splice(i, 1);
+        console.log(data);
+    };
+    return (
+        <div className="App">
+            <button onClick={() => handleData()}>add data</button>
+            <button onClick={() => deleteData(1)}>delete data</button>
+        </div>
+    );
 }
 
 export default App;

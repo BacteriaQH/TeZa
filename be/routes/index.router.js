a6f79f351f4f92c37b31fa1868e102dcb547793a
BacteriaQH <hoangphuc2k1vp@gmail.com>
2022-10-21 16:28:38 +0700

    add router in routes

--- be/routes/index.router.js
+++ be/routes/index.router.js
@@ -1,12 +1,21 @@
 import express from 'express';
-
 const router = express.Router();
 
-import { Greeting } from '../controllers/user.controller.js';
+import {
+    CheckEmailController,
+    Greeting,
+    LoginWithPasswordController,
+    RegisterController,
+} from '../controllers/user.controller.js';
+import { CreateOTPController } from '../controllers/otp.controller.js';
 
 const initWebRoutes = (app) => {
     router.get('/hello', Greeting);
+    router.post('/check/mail', CheckEmailController);
+    router.post('/register', RegisterController);
+    router.post('/login/password', LoginWithPasswordController);
 
+    router.post('/generate/otp', CreateOTPController);
     return app.use('/api', router);
 };
 

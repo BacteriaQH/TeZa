b572112f63efe09b947795c6da33c229883e0d12
BacteriaQH <hoangphuc2k1vp@gmail.com>
2022-12-05 17:41:34 +0700

    restructure app

--- TeZaApp/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    // resolver: {
-    //   sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx']
-    // }
-  };
-};
